0e18c4618251e734e32791b0455c83f1c1b26e2e
Florian Gomis <florian.gomis@viacesi.fr>
2018-04-06 14:58:40 +0100

    :art: Improve style

--- src/components/data-dashboard/widget-weather/widgetweather.js
+++ src/components/data-dashboard/widget-weather/widgetweather.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { stringToIcon } from '../../../utility/utility';
+import { stringToIcon, formatDate } from '../../../utility/utility';
 
 const WidgetWeather = (props) => {
     const { date, temp, weather } = props;
     return (
         <div className="widget-container">
             <div>
-                { date }
+                { formatDate(date) }
             </div>
-
-            <div>
+            <div style={ { marginTop: '20px'} }>
                 { temp } °C
             </div>
-
             <div>
                 <img alt="weather icon" src={ stringToIcon(weather) } style={ { width: '20px', height: '20px', marginTop: '20px'} } />
             </div>
