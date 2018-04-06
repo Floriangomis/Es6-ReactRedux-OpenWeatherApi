17ff0411517e74d8c94559e9ddb8876d19e0197a
Floriangomis <florian.gomis@viacesi.fr>
2018-04-06 23:53:51 +0100

    :art: add Legend to the widget under icon

--- src/components/data-dashboard/widget-weather/widgetweather.js
+++ src/components/data-dashboard/widget-weather/widgetweather.js
@@ -12,7 +12,10 @@ const WidgetWeather = (props) => {
                 { temp } °C
             </div>
             <div>
-                <img alt="weather icon" src={ stringToIcon(weather) } style={ { width: '20px', height: '20px', marginTop: '20px'} } />
+                <img alt="weather icon" src={ stringToIcon(weather) } style={ { width: '40px', height: '40px', marginTop: '20px'} } />
+            </div>
+            <div style={ { fontSize: '12px' } }>
+                { weather }
             </div>
         </div>
     )
